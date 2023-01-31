b8c275269075c1a3c9422b3fdbd3013f1ed74e1e
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-01-31 09:44:09 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export const PhotoFrame = () => {
-   
-}
+const PhotoFrame = (props) => {
+  return (
+    <div className="photoframe">
+      <img src={props.image} alt="img" />
+      <h1 className="caption">{props.title}</h1>
+    </div>
+  );
+};
+export default PhotoFrame;
