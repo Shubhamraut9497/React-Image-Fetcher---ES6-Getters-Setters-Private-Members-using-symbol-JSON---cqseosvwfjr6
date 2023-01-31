3313a9e307ff72b79fe2e013cc26a94d1f8e719f
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-01-31 09:40:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+import Loader from "./Loader";
+import PhotoFrame from "./PhotoFrame";
 const App = () => {
-  
-}
+  const [inputValue, setInputValue] = useState("");
+  const [product, setProduct] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const handleNumber = (e) => {
+    setInputValue(e.target.value);
+  };
+  useEffect(() => {
+    const apifun = async () => {
+      setLoader(true);
+      fetch(`https://jsonplaceholder.typicode.com/photos/${inputValue}`)
+        .then((data) => {
+          return data.json();
+        })
+        .then((response) => {
+          setProduct(response);
+          setLoader(false);
+        });
+    };
+    apifun();
+  }, [inputValue]);
 
+  return (
+    <>
+      <label>Id Number :</label>
+      <input
+        type={"number"}
+        id="in"
+        value={inputValue}
+        onChange={handleNumber}
+      />
+      {loader === true ? <Loader /> : null}
+      {inputValue <= 0 ? null : (
+        <PhotoFrame image={product.url} title={product.title} />
+      )}
+    </>
+  );
+};
 
 export default App;
+
